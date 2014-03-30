7cca62c116a77ee549ff54aac9067a341da4ca85
Dan Scott <dan@coffeecode.net>
2014-03-30 01:38:54 -0400

    Handle cStringIO in Python3 test
    Signed-off-by: Dan Scott <dan@coffeecode.net>

--- test/test_aggregate_graphs.py
+++ test/test_aggregate_graphs.py
@@ -3,7 +3,13 @@ from rdflib.namespace import Namespace, RDF, RDFS
 from rdflib import plugin
 from rdflib.term import URIRef
 from rdflib.store import Store
-from cStringIO import StringIO
+from six import PY3
+
+if PY3:
+    from io import StringIO
+else:
+    from cStringIO import StringIO
+
 from rdflib.graph import Graph
 from rdflib.graph import ReadOnlyGraphAggregate
 from rdflib.graph import ConjunctiveGraph
